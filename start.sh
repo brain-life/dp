0f9041e1aab8178f171f1a1a0c47e5cafdb7b955
Soichi Hayashi <hayashis@iu.edu>
2018-04-14 17:35:34 -0400

    updated to skip already existing output

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 echo "generating parameter list"
 true > params.list
-for alpha_v in `seq 0 0.4 7.2`; do
+for alpha_v in `seq -f '%g' 0 0.4 7.2`; do
     alpha_f=0
-    for lambda_1 in `seq 1.0 0.25 2.5`; do
-        for lambda_2 in `seq 0 0.05 0.2`; do
+    for lambda_1 in `seq -f '%g' 1.0 0.25 2.5`; do
+        for lambda_2 in `seq -f '%g' 0 0.05 0.2`; do
             echo $alpha_v $alpha_f $lambda_1 $lambda_2 >> params.list
         done
     done
