2c093c3ad430f7a873f1bda9f78ee8908f94be8b
Soichi Hayashi <hayashis@iu.edu>
2018-05-08 12:26:58 -0400

    updated paarmeter set generator

--- start.sh
+++ start.sh
@@ -9,14 +9,16 @@ rm jobid.best
 
 echo "generating parameter list"
 true > params.list
-for alpha_v in `seq -f '%g' 0 0.4 7.2`; do
+for alpha_v in `seq -f '%g' 0 1.0 14.4`; do
     alpha_f=0
-    for lambda_1 in `seq -f '%g' 1.0 0.25 2.5`; do
-        for lambda_2 in `seq -f '%g' 0 0.05 0.2`; do
+    for lambda_1 in `seq -f '%g' 1.0 0.5 5.0`; do
+        for lambda_2 in `seq -f '%g' 0 0.1 0.5`; do
             echo $alpha_v $alpha_f $lambda_1 $lambda_2 >> params.list
         done
     done
 done
+
+exit
 
 #archive old logs
 if [ -d logs ]; then
@@ -47,10 +49,5 @@ echo "submitting fint_best"
 best=$(sbatch --parsable -c 16 --dependency=afterok:$fit -o "logs/slurm-%j.log" -e "logs/slurm-%j.err" find_best.sh)
 echo $best > jobid.best
 
-#else
-#	echo "no sbatch.. guessing it's running on a test machine"
-#	#simulate sbatch with one task ID
-#	SLURM_ARRAY_TASK_ID=665 nohup ./fit_model.sh &
-#fi
 
 
