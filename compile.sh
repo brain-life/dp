ac6f56901f41faa70c711f4e034f6bc2af731b72
Soichi Hayashi <hayashis@iu.edu>
2018-06-15 15:12:18 -0400

    recompiled compute_profiles

--- compile.sh
+++ compile.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cat > build.m <<END
 %addpath(genpath('/home/hayashis/git/encode'));
-
 addpath(genpath('./encode'));
 addpath(genpath('/home/hayashis/git/vistasoft'));
+addpath(genpath('/home/hayashis/git/mba'))
 addpath(genpath('/home/hayashis/git/jsonlab'));
+
 %mcc -m -R -nodisplay -d compiled fit_model
 %mcc -m -R -nodisplay -d compiled find_best
 mcc -m -R -nodisplay -d compiled compute_profiles
