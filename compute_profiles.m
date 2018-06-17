d77514c77db75a0cfb0c1d97b7512aef6b1fcf77
Soichi Hayashi <hayashis@iu.edu>
2018-06-16 20:54:07 -0400

    updated to remove results / output for compute_profiles

--- compute_profiles.m
+++ compute_profiles.m
@@ -1,8 +1,4 @@
 function [] = compute_profiles()
-%% 
-% 1 - Generate nifti with prdiction of signal given the pair of tracts CST_L and SLF_L
-% 2 - Use VITASOFT to compute FA on predictions
-
 
 if ~isdeployed
     %addpath(genpath('/home/hayashis/git/encode'));
@@ -15,6 +11,8 @@ if ~isdeployed
     addpath(genpath('/N/dc2/projects/lifebid/code/mba/'))
 end
 
+%following is needed to tell mcc compiler that we need to include sptensor even though we
+%don't reference it anywhere in the code
 %# function sptensor
 
 config = loadjson('config.json')
@@ -35,43 +33,36 @@ info.output = struct;
 info.output.niftis = 'output';
 tract1 = 'CST_L';
 tract2 = 'SLF_L';
-%bvals = dlmread(bvalsFile);
 
-if exist(info.output.niftis,'dir') == 7
-    disp('nifti already genrated? skipping part 1/2')
-else
-    mkdir('output');
+disp('step 1 - Generate nifti with prdiction of signal given the pair of tracts CST_L and SLF_L')
+other_tracts = {'ARC_L','Thal_Rad_L'};
+Gen_niftis_crossing_tracts(info, tract1, tract2, other_tracts)
 
-    disp('step 1 - Generate predictions');
-    other_tracts = {'ARC_L','Thal_Rad_L'};
-    Gen_niftis_crossing_tracts(info, tract1, tract2, other_tracts)
+disp('step 2 - Use VITASOFT to compute FA on predictions')
+listing = dir(strcat('output/*.nii.gz'));
+Nfiles = size(listing,1);
+bs.n = 500;
+[bs.permuteMatrix, ~, ~] = dtiBootGetPermMatrix(dlmread(bvecsFile),dlmread(bvalsFile));
+bs.showProgress = false;
 
-    disp('step 2- Comute FAs using VISTASOFT');
-    listing = dir(strcat('output/*.nii.gz'));
-    Nfiles = size(listing,1);
-    bs.n = 500;
-    [bs.permuteMatrix, ~, ~] = dtiBootGetPermMatrix(dlmread(bvecsFile),dlmread(bvalsFile));
-    bs.showProgress = false;
+ni = niftiRead(aligned_dwi);
+for n=1:Nfiles
+    dwRawAligned = fullfile('output',listing(n).name);
+    data_out_path = fullfile(info.output.niftis);
+    [dt6FileName]= dtiRawFitTensorMex(dwRawAligned, bvecsFile, bvalsFile, data_out_path, bs,[],'ls', [], [], 1);
+    dt = dtiLoadDt6(dt6FileName);
+    val_dt6 = dt.dt6;
+    [nil, eigVal] = dtiSplitTensor(val_dt6);
+    val1 = dtiComputeFA(eigVal);
 
-    ni = niftiRead(aligned_dwi);
-    for n=1:Nfiles
-        dwRawAligned = fullfile('output',listing(n).name);
-        data_out_path = fullfile(info.output.niftis);
-        [dt6FileName]= dtiRawFitTensorMex(dwRawAligned, bvecsFile, bvalsFile, data_out_path, bs,[],'ls', [], [], 1);
-        dt = dtiLoadDt6(dt6FileName);
-        val_dt6 = dt.dt6;
-        [nil, eigVal] = dtiSplitTensor(val_dt6);
-        val1 = dtiComputeFA(eigVal);
-
-        %% Generate nifti with FA values
-        name = fullfile('output',strcat('FA_',listing(n).name));
-        
-        ni_out = ni;
-        ni_out.fname = name;
-        ni_out.dim = size(val1);
-        ni_out.data = val1;
-        niftiWrite(ni_out,name);
-    end
+    %% Generate nifti with FA values
+    name = fullfile('output',strcat('FA_',listing(n).name));
+    
+    ni_out = ni;
+    ni_out.fname = name;
+    ni_out.dim = size(val1);
+    ni_out.data = val1;
+    niftiWrite(ni_out,name);
 end
 
 disp('3- Compute and plot profiles');
