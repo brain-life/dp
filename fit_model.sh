4e27cdd8700e9d8316c212b262aea5b917124bd1
Soichi Hayashi <hayashis@iu.edu>
2018-07-04 17:08:11 -0400

    switching the way MAXMEM is set

--- fit_model.sh
+++ fit_model.sh
@@ -32,8 +32,8 @@ fi
 
 echo "generating alpha_v_${alpha_v}_alpha_f_${alpha_f}_lambda_1_${lambda_1}_lambda_2_${lambda_2}.mat"
 #time matlab -nodisplay -nosplash -r "fit_model($alpha_v, $alpha_f, $lambda_1, $lambda_2); exit"
-#export SINGULARITYENV_MAXMEM=16000000
-export MAXMEM=16000000
+export SINGULARITYENV_MAXMEM=16000000
+#export MAXMEM=16000000
 time singularity exec docker://brainlife/mcr:neurodebian1604-r2017a ./compiled/fit_model $alpha_v $alpha_f $lambda_1 $lambda_2
 
 #an attempt to make sure parpool clean up itself
